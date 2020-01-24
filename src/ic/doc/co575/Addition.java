f3db4ac1e7d28dd10be08ea8cf34f804ffbf5857
bti19 <bti19@ic.ac.uk>
2020-01-24 11:34:43 +0000

    BI BP - wrote Addition and Product subclasses

---
+++ src/ic/doc/co575/Addition.java
@@ -0,0 +1,36 @@
+package ic.doc.co575;
+
+public class Addition extends BinaryExpression {
+
+    private final Expression leftExpression;
+    private final Expression rightExpression;
+
+    public Addition(Expression left, Expression right)
+    {
+        leftExpression = left;
+        rightExpression = right;
+    }
+
+    @Override
+    public int evaluate() {
+        return this.getLeft().evaluate() + this.getRight().evaluate();
+    }
+
+    @Override
+    public int depth() {
+        return this.getLeft().depth() + this.getRight().depth() + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.getRight().toString() + "+" + this.getLeft().toString() + ")";
+    }
+
+    public Expression getLeft() {
+        return leftExpression;
+    }
+
+    public Expression getRight() {
+        return rightExpression;
+    }
+}
---
+++ src/ic/doc/co575/BinaryExpression.java
@@ -0,0 +1,5 @@
+package ic.doc.co575;
+
+public abstract class BinaryExpression implements Expression {
+
+}
